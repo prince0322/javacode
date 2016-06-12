289c0a50a02649968546a723848c6c15d014b3c0
Prince Raj Kumar <princer@google.com>
2016-06-12 20:32:17 +0530

    My template

---
+++ src/Template.java
@@ -0,0 +1,60 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.io.InputStream;
+
+public class Template {
+
+  public static void main(String[] args) {
+    InputStream inputStream = System.in;
+    OutputStream outputStream = System.out;
+    InputReader in = new InputReader(inputStream);
+    PrintWriter out = new PrintWriter(outputStream);
+    MainTask solver = new MainTask();
+    solver.solve(in, out);
+    out.close();
+  }
+
+  static class MainTask {
+    public void solve(InputReader in, PrintWriter out) {
+      int test = in.nextInt();
+      while (test-- > 0) {
+        int x = in.nextInt();
+        out.println(x);
+      }
+    }
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int nextInt() {
+      return Integer.parseInt(next());
+    }
+  }
+}
